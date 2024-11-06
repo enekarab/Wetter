b2eaa766afc347fa996a47a708a18cd1bfaef779
enekarab <116077816+enekarab@users.noreply.github.com>
2024-11-06 21:24:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function fetchWeather() {
     var city = document.getElementById('city-input').value;
-    var apiKey = `bab281d79e5f1e9755a68d754cc313e7`; // API anahtarınızı buraya ekleyin
+    var apiKey = `bab281d79e5f1e9755a68d754cc313e7`;
     var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=de`;
 
     fetch(apiUrl)
@@ -20,7 +20,7 @@ function fetchWeather() {
 
 function displayWeather(data) {
     var weatherOutput = document.getElementById('weather-output');
-    var appMain = document.querySelector('.app-main'); // Arka planı değiştirmek için ana div'i seçiyoruz
+    var appMain = document.querySelector('.app-main');
 
     if (data && data.weather && data.main) {
         weatherOutput.innerHTML = `
@@ -30,7 +30,7 @@ function displayWeather(data) {
         `;
         weatherOutput.style.display = 'block';
 
-        // Hava durumu türleri ve arka plan resimleri için bir nesne oluşturuyoruz
+       
         var weatherBackgrounds = {
             clear: "url('./clear.png')",
             clouds: "url('./cloud.png')",
@@ -41,11 +41,11 @@ function displayWeather(data) {
             sun: "url('./sonne.png')"
         };
 
-        // Hava durumu türünü küçük harfe çevirerek kontrol ediyoruz
+       
         var weatherType = data.weather[0].main.toLowerCase();
-        var backgroundImage = weatherBackgrounds[weatherType] || ""; // Bilinmeyen durumlar için varsayılan değer
+        var backgroundImage = weatherBackgrounds[weatherType] || "";
 
-        // Arka plan resmini `body` elementine ekliyoruz
+        
         document.body.style.backgroundImage = backgroundImage;
         document.body.style.backgroundSize = "cover";
         document.body.style.backgroundPosition = "center";
@@ -53,6 +53,6 @@ function displayWeather(data) {
     } else {
         weatherOutput.innerHTML = `<p>Stadt nicht gefunden.</p>`;
         weatherOutput.style.display = 'block';
-        document.body.style.backgroundImage = ""; // Hatalı veya bulunamayan şehirlerde varsayılan arka plana geç
+        document.body.style.backgroundImage = "";
     }
 }
